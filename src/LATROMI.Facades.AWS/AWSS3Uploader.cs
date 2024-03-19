f3de1d39fdb68d086e8e261467779325530a4b08
Daniel Giacomelli <daniel.giacomelli@latromi.com.br>
2024-03-18 21:58:19 -0300

    Adicionado Projeto para Upload para o AWS S3

---
+++ src/LATROMI.Facades.AWS/AWSS3Uploader.cs
@@ -0,0 +1,121 @@
+﻿using Amazon.S3.Transfer;
+using Amazon.S3;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LATROMI.Facades.AWS
+{
+    public class AWSS3Uploader
+    {
+        private string _accessKeyId;
+        private string _secretAccessKey;
+        private string _bucketName;
+        private string _region;
+
+        public void SetCredenditals(string accessKeyId, string secretAccessKey)
+        {
+            if (string.IsNullOrWhiteSpace(accessKeyId))
+            {
+                throw new ArgumentException($"'{nameof(accessKeyId)}' cannot be null or whitespace.", nameof(accessKeyId));
+            }
+
+            if (string.IsNullOrWhiteSpace(secretAccessKey))
+            {
+                throw new ArgumentException($"'{nameof(secretAccessKey)}' cannot be null or whitespace.", nameof(secretAccessKey));
+            }
+
+            _accessKeyId = accessKeyId;
+            _secretAccessKey = secretAccessKey;
+        }
+
+        public void SetBucket(string bucketName)
+        {
+            if (string.IsNullOrWhiteSpace(bucketName))
+            {
+                throw new ArgumentException($"'{nameof(bucketName)}' cannot be null or whitespace.", nameof(bucketName));
+            }
+
+            _bucketName = bucketName;
+        }
+
+        public void SetRegion(string region)
+        {
+            if (string.IsNullOrWhiteSpace(region))
+            {
+                throw new ArgumentException($"'{nameof(region)}' cannot be null or whitespace.", nameof(region));
+            }
+
+            _region = region;
+        }
+
+        public string UploadFromFile(string filePath, string fileId = null)
+        {
+            using (var uploadStream = File.OpenRead(filePath))
+            {
+                return Upload(uploadStream, fileId);
+            }
+        }
+
+        public string Upload(Stream fileStream, string fileKey = null)
+        {
+            if (fileStream is null)
+            {
+                throw new ArgumentNullException(nameof(fileStream));
+            }
+
+            if (fileKey  is null)
+            {
+                fileKey = Guid.NewGuid().ToString("N");
+            }
+
+            EnsureCredentialsSpecified();
+            EnsureBucketSpecified();
+            EnsureRegionSpecified();
+
+            try
+            {
+                // Obtém Endpoint da região
+                var regionEndPoint = Amazon.RegionEndpoint.GetBySystemName(_region);
+
+                // Cria um objeto de cliente AmazonS3
+                using (var client = new AmazonS3Client(_accessKeyId, _secretAccessKey, regionEndPoint))
+                using (var transferUtility = new TransferUtility(client)) // Cria um objeto TransferUtility
+                {
+                    // Faz o upload do arquivo para o Amazon S3
+                    transferUtility.Upload(fileStream, _bucketName, fileKey);
+                }
+                return fileKey;
+            }
+            catch (AmazonS3Exception)
+            {
+                throw;
+            }
+        }
+
+        private void EnsureCredentialsSpecified()
+        {
+            if (_accessKeyId is null)
+            {
+                throw new InvalidOperationException("Credentials are not provided");
+            }
+        }
+
+        private void EnsureBucketSpecified()
+        {
+            if (_bucketName is null)
+            {
+                throw new InvalidOperationException("Bucket name is not provided");
+            }
+        }
+
+        private void EnsureRegionSpecified()
+        {
+            if (_region is null)
+            {
+                throw new InvalidOperationException("Region is not provided");
+            }
+        }
+    }
+}
